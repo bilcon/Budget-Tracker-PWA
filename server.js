52c341e5ffc777335c3081c3f592def9570415ea
Bill Morris <bilcon@swbell.net>
2022-02-09 12:07:40 -0600

    Deploying

--- server.js
+++ server.js
@@ -16,10 +16,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // routes
